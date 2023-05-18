055ecedeafc899cfbaac2b9135e192ea5b38e80c
arthur <arthur@NT-0000.asq.local>
2023-05-18 00:26:40 -0300

    Implementado melhorias na tela de cadastro

--- src/main/java/view/TelaCadastroEndereco.java
+++ src/main/java/view/TelaCadastroEndereco.java
@@ -10,6 +10,7 @@ import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class TelaCadastroEndereco {
 
@@ -20,7 +21,7 @@ public class TelaCadastroEndereco {
 	private JTextField txtBairro;
 	private JTextField txtCidade;
 	private JTextField txtEstado;
-	private JButton btnSave;
+	private JButton btnSalvar;
 	
 	//TESTE DE ATUALIZACAO GIT
 
@@ -52,7 +53,9 @@ public class TelaCadastroEndereco {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 498, 328);
+		frame.getContentPane().setBackground(new Color(68, 82, 115));
+		frame.setBackground(new Color(255, 255, 255));
+		frame.setBounds(100, 100, 498, 283);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
 				FormSpecs.RELATED_GAP_COLSPEC,
@@ -77,9 +80,12 @@ public class TelaCadastroEndereco {
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,
 				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
 		JLabel lblCep = new JLabel("CEP");
+		lblCep.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblCep, "2, 4");
 		
 		txtCep = new JTextField();
@@ -87,6 +93,7 @@ public class TelaCadastroEndereco {
 		txtCep.setColumns(10);
 		
 		JLabel lblRua = new JLabel("RUA");
+		lblRua.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblRua, "2, 6");
 		
 		txtRua = new JTextField();
@@ -94,6 +101,7 @@ public class TelaCadastroEndereco {
 		frame.getContentPane().add(txtRua, "5, 6, fill, default");
 		
 		JLabel lblNumero = new JLabel("NUMERO");
+		lblNumero.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblNumero, "2, 8");
 		
 		txtNumero = new JTextField();
@@ -101,6 +109,7 @@ public class TelaCadastroEndereco {
 		frame.getContentPane().add(txtNumero, "5, 8, fill, default");
 		
 		JLabel lblBairro = new JLabel("BAIRRO");
+		lblBairro.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblBairro, "2, 10");
 		
 		txtBairro = new JTextField();
@@ -108,6 +117,7 @@ public class TelaCadastroEndereco {
 		frame.getContentPane().add(txtBairro, "5, 10, fill, default");
 		
 		JLabel lblCidade = new JLabel("CIDADE");
+		lblCidade.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblCidade, "2, 12");
 		
 		txtCidade = new JTextField();
@@ -115,14 +125,18 @@ public class TelaCadastroEndereco {
 		frame.getContentPane().add(txtCidade, "5, 12, fill, default");
 		
 		JLabel lblEstado = new JLabel("ESTADO");
+		lblEstado.setForeground(new Color(255, 255, 255));
+		lblEstado.setBackground(new Color(255, 255, 255));
 		frame.getContentPane().add(lblEstado, "2, 14");
 		
 		txtEstado = new JTextField();
 		txtEstado.setColumns(10);
 		frame.getContentPane().add(txtEstado, "5, 14, fill, default");
 		
-		btnSave = new JButton("Salvar");
-		frame.getContentPane().add(btnSave, "2, 16, 4, 1");
+		btnSalvar = new JButton("SALVAR");
+		btnSalvar.setForeground(new Color(255, 255, 255));
+		btnSalvar.setBackground(new Color(89, 109, 56));
+		frame.getContentPane().add(btnSalvar, "2, 18, 4, 1, fill, bottom");
 	}
 
 }
