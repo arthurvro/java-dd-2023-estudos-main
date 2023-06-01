a03d72b69e216f81817e6f93185722380ae92698
arthur <arthur@NT-0000.asq.local>
2023-05-31 21:05:53 -0300

    Atualizacao de testeTela

---
+++ src/main/java/view/TestePainel.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class TestePainel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TestePainel() {
+		
+		JPanel panel = new JPanel();
+		add(panel);
+
+	}
+
+}
---
+++ src/main/java/view/TesteTela.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TesteTela {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TesteTela window = new TesteTela();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TesteTela() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(24, 10, 387, 233);
+		frame.getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnNewButton);
+	}
+}
